538f2a97bd65c70f6d70a241f865efb9fddebf99
bof195 <113489791+bof195@users.noreply.github.com>
2023-04-21 15:47:23 +0700

    feat: footer (#20)

--- src/components/components/footer.js
+++ src/components/components/footer.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+// import { Link } from "react-router-dom";
 
-const footer= () => (
+const footer = () => (
   <footer className="footer-light">
-            <div className="container">
+    {/* <div className="container">
                 <div className="row">
                     <div className="col-md-3 col-sm-6 col-xs-1">
                         <div className="widget">
@@ -62,34 +62,67 @@ const footer= () => (
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="subfooter">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="de-flex">
-                                <div className="de-flex-col">
-                                    <span onClick={()=> window.open("", "_self")}>
-                                        <img alt="" className="f-logo d-1" src="./img/logo.png" />
-                                        <img alt="" className="f-logo d-3" src="./img/logo-2-light.png" />
-                                        <img alt="" className="f-logo d-4" src="./img/logo-3.png" />
-                                        <span className="copy">&copy; Copyright 2023 - NFT Trait Hunting by Lydia Labs</span>
-                                    </span>
-                                </div>
-                                <div className="de-flex-col">
-                                    <div className="social-icons">
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-facebook fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-twitter fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-linkedin fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-pinterest fa-lg"></i></span>
-                                        <span onClick={()=> window.open("", "_self")}><i className="fa fa-rss fa-lg"></i></span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            </div> */}
+    <div className="subfooter">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="de-flex">
+              <div className="de-flex-col">
+                <span onClick={() => window.open("", "_self")}>
+                  <img alt="" className="f-logo d-1" src="./img/logo.png" />
+                  <img
+                    alt=""
+                    className="f-logo d-3"
+                    src="./img/logo-2-light.png"
+                  />
+                  <img alt="" className="f-logo d-4" src="./img/logo-3.png" />
+                  <span className="copy">
+                    &copy; Copyright 2023 - NFT Trait Hunting by Lydia Labs
+                  </span>
+                </span>
+              </div>
+              <div className="de-flex-col">
+                <div className="social-icons">
+                  {/* <span onClick={() => window.open("", "_self")}>
+                    <i className="fa fa-facebook fa-lg"></i>
+                  </span> */}
+
+                  <a
+                    className="btn-website"
+                    href="https://lydialabs.xyz/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span onClick={() => window.open("", "_self")}>
+                      <i className="fa fa-globe fa-lg"></i>
+                    </span>
+                  </a>
+
+                  <a
+                    className="btn-twitter"
+                    href="https://twitter.com/lydia_labs"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span onClick={() => window.open("", "_self")}>
+                      <i className="fa fa-twitter fa-lg"></i>
+                    </span>
+                  </a>
+
+                  {/* <span onClick={() => window.open("", "_self")}>
+                    <i className="fa fa-linkedin fa-lg"></i>
+                  </span> */}
+                  {/* <span onClick={() => window.open("", "_self")}>
+                    <i className="fa fa-pinterest fa-lg"></i>
+                  </span> */}
                 </div>
+              </div>
             </div>
-        </footer>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 );
 export default footer;
