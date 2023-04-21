576a1ebd0deacf1bcd8e588e93e572cb23a248ce
mint-pz <zmint.pz@gmail.com>
2023-04-21 15:43:08 +0700

    feat: #18 merge content

--- src/components/pages/colectionGrey.js
+++ src/components/pages/colectionGrey.js
@@ -53,10 +53,17 @@ const Colection = function () {
   return (
     <div className="greyscheme">
       <StyledHeader theme={theme} />
-      <section className="container" style={{ paddingTop: "200px" }}>
+      <section className="container" style={{ paddingTop: "150px" }}>
         <div className="row">
-          <div className="col-lg-12">
+          <div className="col-lg-7">
             <div className="items_filter mb-5" style={{ marginTop: 0 }}>
+              <h5>Merge Swords</h5>
+              <p className="p-info pb-3" style={{ fontSize: "16px" }}>
+                Using generative AI, two swords are merged to create the next
+                tier sword, which inherits traits of existing two swords. The
+                success rate is 50% for mergin common swords and 30% for rare
+                swords.
+              </p>
               <button
                 onClick={onMergeNFTs}
                 disabled={selectedNfts?.length < 2 || loading}
